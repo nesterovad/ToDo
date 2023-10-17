a4565bbd38e15a150b3ac0bd9eeaf97774af3d18
Нестерова Таисия Григорьевна <tayanesterova91@gmail.com>
2023-10-17 17:07:44 +0700

    added some data for testing

---
+++ todo/src/testData.js
@@ -0,0 +1,16 @@
+const shortTasks = [
+    {
+        id: 0,
+        name: "test task 1",
+        status: 'new'
+    },
+    {
+        id: 1, 
+        name: "test task 2",
+        status: 'new'
+    }
+]
+
+export {
+    shortTasks,
+}
