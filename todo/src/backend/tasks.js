54c0e839140d14aa615ae7e6cb431a8316b994d7
Нестерова Таисия Григорьевна <tayanesterova91@gmail.com>
2023-11-23 17:53:01 +0700

    added getting projects data for preview

--- todo/src/backend/tasks.js
+++ todo/src/backend/tasks.js
@@ -9,7 +9,7 @@ function tasks(action, data){
     let res;
     switch (action){
         case 'get':
-            //todo: add function call for getting tasks with projId
+            res = getTasks(data);
             break;
         case 'update':
             //todo: add function call for updatin status 
@@ -22,6 +22,41 @@ function tasks(action, data){
             break;
     };
     return res;
+}
+
+/**
+ * Функция получения задач для предпросмотра
+ * @param {number} projId  - идентификатор проекта, для которого необходимо получить задачи
+ * @returns {Array} - массив данных задач
+ */
+function getTasks(projId){
+    const tmp = localStorage.getItem("tasks");
+    const allTasks = JSON.parse(tmp);
+    const tasks = allTasks.filter(item => item.projId === projId).map(task => (
+        {
+            projId: task.projId,
+            id: task.id,
+            name: task.name,
+            status: task.status,
+            startDate: task.startDate,
+            createdate: task.createdate,
+            finishDate: task.finishDate,
+            endDate: task.endDate
+        }
+    )
+    ) ;
+    if (!tasks.length){
+        return {
+            status: '404',
+            message: 'Project does not exist'
+        }
+    }
+    return {
+        status: '200',
+        message: 'ok',
+        data: tasks
+    };
+
 }
 
 /**
